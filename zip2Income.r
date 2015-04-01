f919df3af4343f83ad74ab32ac5c933259141115
yeshaq <yossofeshaq@gmail.com>
2015-04-01 16:50:41 -0700

    income column

--- zip2Income.r
+++ zip2Income.r
@@ -1,7 +1,13 @@
 library(acs)
-zipList = c("07652","04106","14215","84404","44122","63109","21114","30132","07030","60187")
-geos=geo.make(zip.code=zipList)
-kTown=acs.fetch(geography=geos, table.number="B19013")
-x = seq(20000,100000,10000)
-print(x)
-findInterval(estimate(kTown),x)
+#zipList = c("07652","04106","14215","84404","44122","63109","21114","30132","07030","60187")
+#geos=geo.make(zip.code=zipList)
+#kTown=acs.fetch(geography=geos, table.number="B19013")
+#x = seq(20000,100000,10000)
+#print(x)
+#findInterval(estimate(kTown),x)
+
+### Given a csv file imported as testQ, a new column is formed which calculates the income from the homezipcode column.  
+
+allZips = acs.fetch(geo = geo.make(zip.code="*"), table.number="B19013", endyear=2012) 
+
+testQ$zipIncome <- apply(testQ['homezipcode'], 1, function(x) { ifelse(x %in% geography(allZips)[["zipcodetabulationarea"]],  estimate(acs.fetch(geo=geo.make(zip.code=as.character(x)),table.number="B19013")), 0)})
